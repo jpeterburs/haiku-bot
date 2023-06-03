2069ffa4dfbff9f0f2d130b39c3e4eae8fd8c4b1
Jonas Peterburs <j.peterburs@protonmail.com>
2023-06-03 10:23:36 +0000

    Fix haiku detection issue

--- main.py
+++ main.py
@@ -2,48 +2,47 @@ import discord
 import syllables
 import os
 
-haiku_form = [5, 7, 5]
+def split_into_haiku(string):
+  words = string.split()
+  haiku = []
+  lines = [[], [], []]  # Use a list to store lines 1, 2, and 3
+
+  syllable_count = 0
+  for word in words:
+    syllable_count += syllables.estimate(word)
+    if syllable_count <= 5:
+      lines[0].append(word)
+    elif syllable_count <= 12:
+      lines[1].append(word)
+    else:
+      lines[2].append(word)
+
+  return [' '.join(line) for line in lines]
+
+def count_syllables(line):
+  words = line.split()
+  return sum(syllables.estimate(word) for word in words)
+
+def is_haiku(lines):
+  expected_syllables = [5, 7, 5]
+  line_syllables = [count_syllables(line) for line in lines]
+
+  return line_syllables == expected_syllables
 
 class Client(discord.Client):
   async def on_message(self, message):
     if message.author.bot:
       return
 
-    syl_count = 0
-    offset = 0
-    i = 0
-    haiku = []
+    poem = split_into_haiku(message.content.replace('\n', ' '))
 
-    for syl_per_line in haiku_form:
-      line = []
+    if is_haiku(poem):
+      haiku = '\n'.join(poem) + '\n'
 
-      if i == 0:
-        words = message.content.split()
-      else:
-        words = message.content.split()[(offset - i):]
-
-      for word in words:
-        syl_count += syllables.estimate(word)
-        line.append(word)
-
-        if syl_count == syl_per_line:
-          syl_count = 0
-          haiku.append(line)
-          break
-        elif syl_count > syl_per_line:
-          return
-
-      offset += syl_per_line
-      i += 1
-
-    reply = ''
-    for ln in haiku:
-      reply += ' '.join(ln) + '\n'
-
-    await message.reply(reply + '\n*Beep noop! Ich halte Ausschau nach versehentlichen Haikus. Manchmal mache ich Fehler.*')
+      await message.reply(haiku + '\n*Beep noop! Ich halte Ausschau nach versehentlichen Haikus. Manchmal mache ich Fehler.*')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = Client(intents=intents)
+client = Client(intents = intents)
 client.run(os.environ['TOKEN'])
